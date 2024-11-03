8b02cd6e8f5bacb604b0ffcd61d01b783bedcaac
neelamyadav500 <144614756+neelamyadav500@users.noreply.github.com>
2024-11-03 10:26:19 -0500

    Update AuthRouter.js

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,18 +1,9 @@
 const { signup, login } = require("../Controllers/AuthController");
 const { loginValidation, signupValidation } = require("../Middlewares/AuthValidation");
 
-const router = require("express").Router();
+const router=require("express").Router();
 
-router.post("/login", loginValidation, async (req, res) => {
-    try {
-        // Pass both req and res to the login function
-        await login(req, res); 
-    } catch (error) {
-        console.error("Login route error:", error);
-        res.status(500).json({ error: error.message });
-    }
-});
-
+router.post("/login", loginValidation, login);
 router.post("/signup", signupValidation, signup);
 
-module.exports = router;
+module.exports=router;
