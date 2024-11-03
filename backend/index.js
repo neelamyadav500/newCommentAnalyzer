57bdcb68cb50328146a0e6b08a32dca7aea36917
neelamyadav500 <144614756+neelamyadav500@users.noreply.github.com>
2024-11-03 10:43:44 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,27 @@
 require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const AuthRouter = require("./Routes/AuthRouter");
-const ProductRouter = require("./Routes/ProductRouter");
-const CommentAnalyzerRouter = require("./Routes/CommentAnalyzerRouter");
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const AuthRouter=require("./Routes/AuthRouter");
+const ProductRouter=require("./Routes/ProductRouter");
+const CommentAnalyzerRouter=require("./Routes/CommentAnalyzerRouter");
 
 require("./Models/db");
 
-const app = express();
-
 app.use(bodyParser.json());
-app.use(cors({
-    origin: "https://new-comment-analyzer-ny.vercel.app", // Your allowed origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // Allow credentials (like cookies or authorization headers)
-}));
+app.use(cors());
+app.use("/auth",AuthRouter);
+app.use("/products",ProductRouter);
+app.use("/comments",CommentAnalyzerRouter);
 
-// Define routes after CORS middleware
-app.use("/auth", AuthRouter);
-app.use("/products", ProductRouter);
-app.use("/comments", CommentAnalyzerRouter);
-
-// Handle preflight requests
-app.options('*', cors());
-
-// Generic error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack); // Log the error stack
-    res.status(500).send('Something broke!'); // Send a generic error message
-});
-
-const PORT = process.env.PORT || 8080;
+const PORT= process.env.PORT || 8080;
 
 app.get("/test", (req, res) => {
     res.send("Test route is working!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+} )
