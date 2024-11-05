20a0bb0c13ff7d728880902a72e6533c24ad2af1
Neelam <neelamyadav4787@gmail.com>
2024-11-04 21:37:22 -0500

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,35 @@
 require("dotenv").config();
-const express=require("express");
-const app=express();
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const AuthRouter=require("./Routes/AuthRouter");
-const ProductRouter=require("./Routes/ProductRouter");
-const CommentAnalyzerRouter=require("./Routes/CommentAnalyzerRouter");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+const ProductRouter = require("./Routes/ProductRouter");
+const CommentAnalyzerRouter = require("./Routes/CommentAnalyzerRouter");
 
 require("./Models/db");
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use("/auth",AuthRouter);
-app.use("/products",ProductRouter);
-app.use("/comments",CommentAnalyzerRouter);
 
-const PORT= process.env.PORT || 8080;
+const corsOptions = {
+    origin: 'https://new-comment-analyzer-ny.vercel.app',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.use("/auth", AuthRouter);
+app.use("/products", ProductRouter);
+app.use("/comments", CommentAnalyzerRouter);
+
+const PORT = process.env.PORT || 8080;
 
 app.get("/test", (req, res) => {
     res.send("Test route is working!");
 });
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
-} )
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
