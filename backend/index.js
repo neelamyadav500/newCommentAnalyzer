92ffe24fc84da745d68757c04dbf50eb506cace1
neelamyadav500 <144614756+neelamyadav500@users.noreply.github.com>
2025-02-09 10:51:06 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,21 @@
 require("dotenv").config();
-const express=require("express");
-const app=express();
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const AuthRouter=require("./Routes/AuthRouter");
-const ProductRouter=require("./Routes/ProductRouter");
-const CommentAnalyzerRouter=require("./Routes/CommentAnalyzerRouter");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+const ProductRouter = require("./Routes/ProductRouter");
+const CommentAnalyzerRouter = require("./Routes/CommentAnalyzerRouter");
 
 require("./Models/db");
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/auth",AuthRouter);
-app.use("/products",ProductRouter);
-app.use("/comments",CommentAnalyzerRouter);
-
-const PORT= process.env.PORT || 8080;
+app.use("/auth", AuthRouter);
+app.use("/products", ProductRouter);
+app.use("/comments", CommentAnalyzerRouter);
 
 app.get("/test", (req, res) => {
     res.send("Test route is working!");
 });
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
-} )
+module.exports = app;
